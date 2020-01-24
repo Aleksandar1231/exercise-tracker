46ab8eb412396966487f22749e9248fd24209c98
Aleksandar1231 <aleksandarsavic@cmail.carleton.ca>
2020-01-24 13:51:31 -0500

    routes for db models

--- exercise-tracker/backend/routes/exercises.js
+++ exercise-tracker/backend/routes/exercises.js
@@ -13,6 +13,48 @@ router.route('/').get((req, res) => {
 
 });
 
+//get exercise by id
+router.route("/:id").get((req, res) => {
+    
+    Exercise.findById(req.params.id)
+    .then((exercise)=>res.json(exercise))
+    .catch((err)=>res.status(400).json("Error: "+err))
+});
+
+//update exercise by id
+router.route("/update/:id").post((req,res) => {
+
+    Exercise.findById(req.params.id)
+    .then((exercise) => {
+        const username = req.body.username;
+        const description = req.body.description;
+        const duration = Number(req.body.duration);
+        const date = Date.parse(req.body.date);
+
+        
+        exercise.username=username;
+        exercise.description=description;
+        exercise.duration= duration;
+        exercise.date=date;
+
+        exercise.save()
+        .then(()=> res.json("Exercise is updated"))
+        .catch((err)=>res.status(400).json("Error is:" + err))
+        
+    })
+    .catch((err)=> res.status(400).json("error is: "+err))
+    
+});
+
+//delete exercise
+router.route('/:id').delete((req,res)=>{
+    
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(()=>res.json("Exercise is deleted!"))
+    .catch((err) => res.status(400).json("Error: "+ err))
+
+});
+
 //handle post requets
 router.route('/add').post((req, res) => {
 
@@ -21,15 +63,15 @@ router.route('/add').post((req, res) => {
     const duration = Number(req.body.duration);
     const date = Date.parse(req.body.date);
 
-    const newExercise = ({
+    const newExercise = new Exercise({
         username,
         description,
         duration,
         date
     });
 
-    newExercise.save()
-    .then(() => res.json('Exercise added'))
+    newExercise.save().
+    then(() => res.json('Exercise added'))
     .catch(err => res.status(400).json('Error' + err));
 });
 
