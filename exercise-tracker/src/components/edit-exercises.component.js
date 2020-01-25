de89191284ad831c074ae7c7d5eea42c4059d225
Aleksandar1231 <aleksandarsavic@cmail.carleton.ca>
2020-01-24 20:18:58 -0500

    front end for create users and exercises

---
+++ exercise-tracker/src/components/edit-exercises.component.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+export default class EditExercises extends Component{
+    
+    render(){
+        
+        return(
+            
+            <div>
+            <p>You are on Edit Exercises</p>
+            </div>
+            )
+            
+        }
+        
+    }
---
+++ exercise-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+export default class ExercisesList extends Component{
+    
+    render(){
+        
+        return(           
+            <div>
+            <p>You are on Exercises List</p>
+            </div>
+            )
+            
+        }
+        
+    }
